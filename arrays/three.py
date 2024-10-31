f2122bae9f76f95ff6c66b6d58b2deb7420b1a48
iamlego <sairaj.jadhav812@gmail.com>
2024-10-30 23:30:22 -0700

    complete sln

--- arrays/three.py
+++ arrays/three.py
@@ -1,13 +1,26 @@
-arr = [12, 3, 1, 2, -6, 5, -8, 1]
-t = 0
-
-arr.sort()
-
-i = 0
+array = [12, 3, 1, 2, -6, 5, -8, 1]
+targetSum = 0
 
 
-while(i < len(arr) - 1):
-    left = i + 1
-    right = len(arr) - 1
-    print(arr[left] + arr[right] + arr[i])
-    i = i + 1
+def threeNumberSum(array, targetSum):
+    array.sort()
+    res = []
+
+    for i in range(len(array)):
+        twoSum(array, i, res, targetSum)
+    return res
+
+def twoSum(array, i, res, targetSum):
+    l, r = i+1, len(array)-1
+
+    while l<r:
+        if array[i] + array[l] + array[r] < targetSum:
+            l += 1
+        elif array[i] + array[l] + array[r] > targetSum:
+            r -= 1
+        else:
+            res.append([array[i], array[l], array[r]])
+            l+=1
+    return res
+
+print(threeNumberSum(array, targetSum))
